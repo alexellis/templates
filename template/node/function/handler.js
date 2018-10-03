0e3ef8994681ee59e696bd7843a7fd3442fb3435
Alex Ellis <alexellis2@gmail.com>
2018-10-03 22:29:41 +0300

    await/async template for node
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- template/node/function/handler.js
+++ template/node/function/handler.js
@@ -1,5 +1,14 @@
 "use strict"
 
-module.exports = (context, callback) => {
-    callback(undefined, {status: "done"});
+// Handler: handles a Serverless OpenFaaS request and must implement
+// the `async` keyword.
+//
+// context: contains the raw input
+// return: result of function
+//
+// Use `throw new Error()` for error handling
+module.exports = async function Handler(context) {
+    if (!context) throw new Error(`You must provide a value.`);
+
+    return {"status": "ok"}
 }
--- template/node/index.js
+++ template/node/index.js
@@ -4,23 +4,7 @@
 "use strict"
 
 const getStdin = require('get-stdin');
-
 const handler = require('./function/handler');
-
-getStdin().then(val => {
-    handler(val, (err, res) => {
-        if (err) {
-            return console.error(err);
-        }
-        if(isArray(res) || isObject(res)) {
-            console.log(JSON.stringify(res));
-        } else {
-            process.stdout.write(res);
-        }
-    });
-}).catch(e => {
-    console.error(e.stack);
-});
 
 const isArray = (a) => {
     return (!!a) && (a.constructor === Array);
@@ -29,3 +13,35 @@ const isArray = (a) => {
 const isObject = (a) => {
     return (!!a) && (a.constructor === Object);
 };
+
+// main executes a Serverless request. The result is always written
+// to stderr or stdout. To indicate a non-200 (i.e. 500) status then
+// call `process.exit()` with a non-zero code. To indicate a 200 code
+// then allow the program to exit normally or call `process.exit(0)`.
+async function main() {
+    let stdInput;
+    try {
+        stdInput = await getStdin()
+    } catch (e) {
+        console.error(e, e.Stack);
+        return process.exit(1);
+    }
+
+    try {
+        const res = await handler(stdInput);
+        if(res) {
+            const stringify = isArray(res) || isObject(res);
+
+            if(stringify) {
+                process.stdout.write(`${JSON.stringify(res)}\n`);
+            } else {
+                process.stdout.write(res);
+            }
+        }
+    } catch(e) {
+        console.error(e, e.Stack);
+        return process.exit(1);
+    }
+}
+
+main();
